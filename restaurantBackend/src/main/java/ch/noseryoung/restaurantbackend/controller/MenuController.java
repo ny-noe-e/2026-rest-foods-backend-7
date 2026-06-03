666313080f93fda2cfd0b8ce7989cc610226b81e
ny-margarita-g <margarita.grigoreva@noseryoung.ch>
2026-06-03 11:58:07 +0200

    added first endpoint

--- restaurantBackend/src/main/java/ch/noseryoung/restaurantbackend/controller/MenuController.java
+++ restaurantBackend/src/main/java/ch/noseryoung/restaurantbackend/controller/MenuController.java
@@ -1,4 +1,21 @@
 package ch.noseryoung.restaurantbackend.controller;
 
+import ch.noseryoung.restaurantbackend.model.Menu;
+import ch.noseryoung.restaurantbackend.service.MenuService;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/menus")
 public class MenuController {
+    @Autowired
+    private MenuService  menuService;
+
+    @GetMapping
+    public ResponseEntity<List<Menu>> getAllMenus() {
+
+        return ResponseEntity.
+                status(HttpStatus.OK).
+                body(menuService.getAllMenus());
+    }
 }
