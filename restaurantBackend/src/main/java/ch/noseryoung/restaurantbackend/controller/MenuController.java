5a74fc4743c922dea3c6c6d5dd1f5de58afd7043
ny-dario-z <dario.zueger@noseryoung.ch>
2026-06-03 12:04:02 +0200

    completed menu domain structure + added imports to MenuController

--- restaurantBackend/src/main/java/ch/noseryoung/restaurantbackend/controller/MenuController.java
+++ restaurantBackend/src/main/java/ch/noseryoung/restaurantbackend/controller/MenuController.java
@@ -2,6 +2,12 @@ package ch.noseryoung.restaurantbackend.controller;
 
 import ch.noseryoung.restaurantbackend.model.Menu;
 import ch.noseryoung.restaurantbackend.service.MenuService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
--- restaurantBackend/src/main/java/ch/noseryoung/restaurantbackend/service/MenuService.java
+++ restaurantBackend/src/main/java/ch/noseryoung/restaurantbackend/service/MenuService.java
@@ -1,4 +1,19 @@
 package ch.noseryoung.restaurantbackend.service;
 
+import ch.noseryoung.restaurantbackend.model.Menu;
+import ch.noseryoung.restaurantbackend.repository.MenuRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class MenuService {
+
+    @Autowired
+    private MenuRepository repository;
+
+    public List<Menu> getAllMenus() {
+        return repository.findAll();
+    }
 }
